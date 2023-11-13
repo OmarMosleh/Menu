8896df248b99a7160518cc8d90104093c4249037
Omar_Mosleh <omarmosleh90@gmail.com>
2023-11-13 09:57:05 +0200

    package.json

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import React, { useState } from "react";
 import Menu from "./Menu";
 import Categories from "./Categories";
 import items from "./data";
-const allCategories = ["all", ... new Set(items.map((item)=>item.category))];
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
 function App() {
   const [menuItems, setMenuItems] = useState(items);
   const [categories, setCategories] = useState(allCategories);
 
-
   const filterItems = (category) => {
-    if (category === "all"){
+    if (category === "all") {
       setMenuItems(items);
       /**
        * //the return; statement is used to exit the function
@@ -19,9 +18,9 @@ function App() {
        */
       return;
     }
-    const newItems = items.filter((item) => item.category === category)
-    setMenuItems(newItems)
-  }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
   return (
     <main>
       <section className="menu section">
