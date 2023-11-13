45ae6e45bd1ec895aa0964c4a96d2245dc8c3d6f
Omar_Mosleh <omarmosleh90@gmail.com>
2023-11-13 09:14:05 +0200

    new commit

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import Menu from "./Menu";
+import Categories from "./Categories";
+import items from "./data";
+const allCategories = ["all", ... new Set(items.map((item)=>item.category))];
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+
+  const filterItems = (category) => {
+    if (category === "all"){
+      setMenuItems(items);
+      /**
+       * //the return; statement is used to exit the function
+       *  early.This is done because, in the case of "all,"
+       *  there's no need to proceed with the rest of the function
+       *  since the desired result has already been achieved.
+       */
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category)
+    setMenuItems(newItems)
+  }
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>Our Menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
